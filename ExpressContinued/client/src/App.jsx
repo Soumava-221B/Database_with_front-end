6200789ca4e998bca027c41bf4f7aac68d44856f
soumava <soumavadas06@gmail.com>
2024-02-15 17:10:20 +0530

    .

---
+++ ExpressContinued/client/src/App.jsx
@@ -0,0 +1,17 @@
+
+function App() {
+
+  return (
+    <>
+      <h1>Admin dashboard portal</h1>
+      <form action="">
+        <input type="text" placeholder="Enter first name" /> <br />
+        <input type="text" placeholder="Enter last name" /> <br />
+        <input type="email" placeholder="Enter email" /> <br />
+        <button></button>
+      </form>
+    </>
+  )
+}
+
+export default App
